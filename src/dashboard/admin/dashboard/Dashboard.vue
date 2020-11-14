268a5eefba9ac12bfdac599c4bffa265f575d146
willypelz <pelumiasefon@gmail.com>
2020-11-14 06:19:38 +0100

    [IMP] add config and dashboard structure

---
+++ src/dashboard/admin/dashboard/Dashboard.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+<h1>dasdborad</h1>
+    </div>
+</template>
+
+<style lang="scss" >
+    @import "./dashboard.scss";
+</style>
+
+<script src="./dashboard.js"></script>
---
+++ src/dashboard/admin/layout/Layout.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <div>
+
+      <TheSideBar ref="drawer" @sideBarWasToggled ="drawer = $event" />
+
+<div>mannnn</div>
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  @import "./layout.scss";
+</style>
+
+<script src="./layout.js"></script>
