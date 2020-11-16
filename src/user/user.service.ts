5d3f555899cad4606aa7315f3ec1e631cb8e8830
willypelz <pelumiasefon@gmail.com>
2020-11-16 04:57:12 +0100

    [FIX] implement user services

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+// import {  } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository, getRepository, DeleteResult } from 'typeorm';
 import { UserEntity } from './user.entity';
@@ -7,15 +7,15 @@ const jwt = require('jsonwebtoken');
 import { SECRET } from '../config';
 import { UserRO } from './user.interface';
 import { validate } from 'class-validator';
-import { HttpException } from '@nestjs/common/exceptions/http.exception';
-import { HttpStatus } from '@nestjs/common';
+// import {  } from '@nestjs/common/exceptions/http.exception';
+import { HttpStatus, HttpException, Injectable } from '@nestjs/common';
 import * as argon2 from 'argon2';
 
 @Injectable()
 export class UserService {
   constructor(
-    @InjectRepository(UserEntity)
-    private readonly userRepository: Repository<UserEntity>
+      @InjectRepository(UserEntity)
+      private readonly userRepository: Repository<UserEntity>
   ) {}
 
   async findAll(): Promise<UserEntity[]> {
@@ -40,9 +40,9 @@ export class UserService {
     // check uniqueness of username/email
     const {username, email, password} = dto;
     const qb = await getRepository(UserEntity)
-      .createQueryBuilder('user')
-      .where('user.username = :username', { username })
-      .orWhere('user.email = :email', { email });
+        .createQueryBuilder('user')
+        .where('user.username = :username', { username })
+        .orWhere('user.email = :email', { email });
 
     const user = await qb.getOne();
 
@@ -57,11 +57,11 @@ export class UserService {
     newUser.username = username;
     newUser.email = email;
     newUser.password = password;
-    newUser.articles = [];
+    newUser.companies = [];
 
     const errors = await validate(newUser);
     if (errors.length > 0) {
-      const _errors = {username: 'Userinput is not valid.'};
+      const _errors = {username: 'User input is not valid.'};
       throw new HttpException({message: 'Input data validation failed', _errors}, HttpStatus.BAD_REQUEST);
 
     } else {
@@ -74,7 +74,6 @@ export class UserService {
   async update(id: number, dto: UpdateUserDto): Promise<UserEntity> {
     let toUpdate = await this.userRepository.findOne(id);
     delete toUpdate.password;
-    delete toUpdate.favorites;
 
     let updated = Object.assign(toUpdate, dto);
     return await this.userRepository.save(updated);
@@ -118,9 +117,7 @@ export class UserService {
       id: user.id,
       username: user.username,
       email: user.email,
-      bio: user.bio,
-      token: this.generateJWT(user),
-      image: user.image
+      token: this.generateJWT(user)
     };
 
     return {user: userRO};
