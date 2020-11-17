bf14a87b64c33ce5fa12f3766c36a117589948b4
willypelz <pelumiasefon@gmail.com>
2020-11-17 10:41:42 +0100

    [IMP]  add registration component

--- src/home/register/Register.vue
+++ src/home/register/Register.vue
@@ -1,5 +1,64 @@
 <template>
-<h1>register</h1>
+  <div>
+    <!-- Header -->
+    <div class="header bg-gradient-success py-7 py-lg-8 pt-lg-9">
+      <div class="separator separator-bottom separator-skew zindex-100">
+        <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1"
+             xmlns="http://www.w3.org/2000/svg">
+          <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
+        </svg>
+      </div>
+    </div>
+    <!-- Page content -->
+    <b-container class="mt--8 pb-5">
+      <!-- Table -->
+      <b-row class="justify-content-center">
+        <b-col lg="6" md="8" >
+          <b-card no-body class="bg-secondary border-0">
+            <b-card-body class="px-lg-5 py-lg-5">
+              <div class="text-center text-muted mb-4">
+               Register
+              </div>
+              <validation-observer v-slot="{handleSubmit}" ref="formValidator">
+                <b-form role="form" @submit.prevent="handleSubmit(onSubmit)">
+                  <base-input alternative
+                              class="mb-3"
+                              prepend-icon="ni ni-hat-3"
+                              placeholder="Username"
+                              name="Username"
+                              :rules="{required: true}"
+                              v-model="model.username">
+                  </base-input>
+
+                  <base-input alternative
+                              class="mb-3"
+                              prepend-icon="ni ni-email-83"
+                              placeholder="Email"
+                              name="Email"
+                              :rules="{required: true, email: true}"
+                              v-model="model.email">
+                  </base-input>
+
+                  <base-input alternative
+                              class="mb-3"
+                              prepend-icon="ni ni-lock-circle-open"
+                              placeholder="password"
+                              type="password"
+                              name="Password"
+                              :rules="{required: true, min: 6}"
+                              v-model="model.password">
+                  </base-input>
+                  <div class="text-center">
+                    <b-button type="submit" variant="primary" class="mt-4">Create account</b-button>
+                  </div>
+                </b-form>
+              </validation-observer>
+            </b-card-body>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script src="./register.js"></script>
