13605117fa023d26812f2cf6bcfca90eb8f4b316
willypelz <pelumiasefon@gmail.com>
2020-11-17 08:51:24 +0100

    [IMP] update the mutation type

--- src/store/type/mutations.type.js
+++ src/store/type/mutations.type.js
@@ -1,3 +1,8 @@
-/* users mutations */
-export const SET_APPLICATION_USERS = "setApplicationUsers";
-export const SET_USER_INFORMATION = "setUserInformation";
+/* Company mutations */
+export const SET_COMPANIES = "set_companies";
+export const SET_COMPANY = "set_company";
+
+
+/* Users mutations */
+export const SET_USERS = "set_users";
+export const SET_USER = "set_user";
