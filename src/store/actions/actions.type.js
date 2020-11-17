41760ad0f39243a9e3a616679ebc97433a60c393
willypelz <pelumiasefon@gmail.com>
2020-11-17 10:39:10 +0100

    [IMP] update the action

--- src/store/actions/actions.type.js
+++ src/store/actions/actions.type.js
@@ -1,4 +1,17 @@
 /*users */
-export const GET_APPLICATION_USERS = "get_application_users";
-export const GET_USER_INFORMATION = "get_user_information";
-export const UPDATE_USER_INFORMATION = "update_user_information";
+export const REGISTER_USER = "register_user";
+export const LOGIN_USER = "login_user";
+export const LOGOUT_USER = "logout_user";
+export const GET_COMPANIES = "get_companies";
+export const GET_ACTIVATED_REVIEW = "get_activated_review";
+export const GET_COMPANY = "get_company";
+export const CREATE_COMPANY = "create_company";
+export const DELETE_COMPANY = "delete_company";
+
+
+
+export const GET_REVIEWS = "get_companies";
+export const GET_REVIEW = "get_company";
+export const UPDATE_REVIEW = "update_company";
+export const CREATE_REVIEW = "create_review";
+export const DELETE_REVIEW = "delete_review";
